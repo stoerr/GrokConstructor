8ae530b912cd78a907244a4d5c4d13f15265d4db
stoerr <KYaLm9tsZbTNcuUhOr8M>
2013-02-26 21:47:20 +0100

    multlining test, but does not work yet

--- src/test/scala/net/stoerr/grokdiscoverytoo/matcher/MultlineFormTest.scala
+++ src/test/scala/net/stoerr/grokdiscoverytoo/matcher/MultlineFormTest.scala
@@ -1,10 +1,35 @@
 package net.stoerr.grokdiscoverytoo.matcher
 
+import org.scalatest.junit.JUnitRunner
+import org.junit.runner.RunWith
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.FlatSpec
+import javax.servlet.http.HttpServletRequest
+import org.scalatest.mock.MockitoSugar
+import org.mockito.Mockito._
+
 /**
- * Tests the functionality of Mult
+ * Tests the functionality of MultlineForm
  * @author <a href="http://www.stoerr.net/">Hans-Peter Stoerr</a>
  * @since 26.02.13
  */
-class MultlineFormTest {
+@RunWith(classOf[JUnitRunner])
+class MultlineFormTest extends FlatSpec with ShouldMatchers with MockitoSugar {
+
+  private class MockForm(val request: HttpServletRequest) extends MultlineForm
+
+  "MultlineForm.multlineFilter" should "correctly parse multlines" in {
+    val mockreq = mock[HttpServletRequest]
+    when(mockreq.getParameter("multline")).thenReturn("^-")
+    val form = new MockForm(mockreq)
+    form.multlineFilter(List()) should equal(List())
+    form.multlineFilter(List("a")) should equal(List("a"))
+    form.multlineFilter(List("-a")) should equal(List("-a"))
+    form.multlineFilter(List("a", "b")) should equal(List("a", "b"))
+    form.multlineFilter(List("a", "b")) should equal(List("-a", "b"))
+    form.multlineFilter(List("a", "-a")) should equal(List("a\n-a"))
+    form.multlineFilter(List("a", "-a", "b")) should equal(List("a\n-a", "b"))
+    form.multlineFilter(List("-a", "b", "-b", "-bb")) should equal(List("-a", "b\n-b\n-bb"))
+  }
 
 }
